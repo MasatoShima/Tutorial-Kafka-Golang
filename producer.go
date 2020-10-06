b526b5fcb065f7f94679370a7622c856c13c7ac2
MasatoShima <masato.shima@icloud.com>
2020-10-07 08:23:22 +0900

    Update

--- producer.go
+++ producer.go
@@ -8,5 +8,41 @@ import (
 )
 
 func main() {
-	producer, err = kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "localhost"})
+	producer, err := kafka.NewProducer(&kafka.ConfigMap{
+		"bootstrap.servers": "localhost",
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer producer.Close()
+
+	// Delivery report handler for produced messages
+	go func() {
+		for event := range producer.Events() {
+			switch e := event.(type) {
+			case *kafka.Message:
+				if e.TopicPartition.Error == nil {
+					fmt.Println("Delivery success \n ", e.TopicPartition)
+				} else {
+					fmt.Println("Delivery failed \n ", e.TopicPartition)
+				}
+			}
+		}
+	}()
+
+	// Produce messages to topic (asynchronously)
+	topic := "TopicName"
+
+	for _, message := range []string{"message1", "message2", "message3"} {
+		producer.Produce(&kafka.Message{
+			TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+			Value:          []byte(message),
+		}, nil)
+	}
+
+	producer.Flush(15 * 1000)
 }
+
+// End
