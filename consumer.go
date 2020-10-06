346ebed85badbf4adbe6fac6405755c98829a3b4
MasatoShima <masato.shima@icloud.com>
2020-10-07 08:23:42 +0900

    新規作成

---
+++ consumer.go
@@ -0,0 +1,39 @@
+// 2020/10/06
+
+package main
+
+import (
+	"fmt"
+	"gopkg.in/confluentinc/confluent-kafka-go.v1/kafka"
+)
+
+func main() {
+	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": "localhost",
+		"group.id":          "myGroup",
+		"auto.offset.reset": "earliest",
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	consumer.SubscribeTopics([]string{"myTopic"}, nil)
+
+	for {
+		message, err := consumer.ReadMessage(-1)
+		if err == nil {
+			fmt.Printf(
+				"Consumer error Topic: %s \n",
+				message.TopicPartition,
+			)
+		} else {
+			fmt.Printf(
+				"Received message Topic: %s Message: %s \n",
+				message.TopicPartition,
+				string(message.Value),
+			)
+		}
+	}
+
+}
