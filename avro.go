446b1e460879957c576bdc966b94e0354ea92ea7
MasatoShima <masato.shima@icloud.com>
2020-10-13 17:50:07 +0900

    新規作成

---
+++ avro.go
@@ -0,0 +1,39 @@
+/*
+Name: avro.go
+Created by: Masato Shima
+Created on: 2020/10/13
+Description:
+Apache avro sample
+*/
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	//"github.com/linkedin/goavro"
+)
+
+func main() {
+	// Read json file
+	file, errReadFile := ioutil.ReadFile("data/sample_1.json")
+
+	if errReadFile != nil {
+		panic(errReadFile)
+	}
+
+	fmt.Printf("%s", file)
+
+	// Encode json file
+	var jsonData interface{}
+
+	errJsonUnmarshal := json.Unmarshal(file, &jsonData)
+
+	if errJsonUnmarshal != nil {
+		panic(errJsonUnmarshal)
+	}
+
+	fmt.Printf("%s", jsonData)
+}
+
+// End
