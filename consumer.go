deba9734a2544e80c444ce3f16a547e3e41a4c37
MasatoShima <masato.shima@quick.jp>
2020-11-02 14:45:48 +0900

    Updated

--- consumer.go
+++ consumer.go
@@ -8,19 +8,17 @@ import (
 )
 
 const (
-	host         = "10.2.152.95"
-	portBrokers  = "9092"
-	portRegistry = "8081"
-	topic        = "SKDB.public.sdcocdmst"
+	host  = "10.2.152.95"
+	port  = "9092"
+	topic = "SKDB.public.sdcocdmst"
 )
 
 func main() {
 	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
-		"bootstrap.servers":   fmt.Sprintf("%s:%s", host, portBrokers),
-		"group.id":            "TEST",
-		"enable.auto.commit":  false,
-		"auto.offset.reset":   "earliest",
-		"schema.registry.url": fmt.Sprintf("http://%s:%s", host, portRegistry),
+		"bootstrap.servers":  fmt.Sprintf("%s:%s", host, port),
+		"group.id":           "TEST",
+		"enable.auto.commit": false,
+		"auto.offset.reset":  "earliest",
 	})
 
 	if err != nil {
